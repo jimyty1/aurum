5e243e7203bac666613802c27660696c604c0bcc
Juan <juan.monterodonoso.2021@live.rhul.ac.uk>
2025-05-27 21:12:37 +0100

    changed navigation layout

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,7 @@
-// File: src/components/Navbar.js
 import React from 'react';
-import {Link, NavLink} from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import '../css/NavBar.css';
-import logo from '../assets/logo new copy.png'
+import logo from '../assets/logo new copy.png';
 
 export default function Navbar() {
   return (
@@ -13,20 +12,12 @@ export default function Navbar() {
         <li><NavLink to="/profile">Profile</NavLink></li>
         <li><NavLink to="/login">Log In</NavLink></li>
       </ul>
-      <Link to={'/'}>
-        <div className='logo-wrapper'>
-          <img
-            className='logo'
-            src={logo}
-            style={{
-              height: '200px',
-              position: 'absolute',
-              left:'-30px',
-            }}
-            alt='logo'
-          />
-        </div>
-      </Link>
+
+      <div className="logo-wrapper">
+        <Link to="/">
+          <img className="logo" src={logo} alt="logo" />
+        </Link>
+      </div>
     </nav>
   );
 }
